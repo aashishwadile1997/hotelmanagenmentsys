eb6361258299f6c1f784086160d0120a1c734948
aashishwadile1997 <108234053+aashishwadile1997@users.noreply.github.com>
2022-09-28 12:30:41 +0530

    Delete Back End directory

--- Back End/src/main/java/com/api/HotelBookingApp/Service/FeedbackService.java
+++
@@ -1,9 +0,0 @@
-package com.api.HotelBookingApp.Service;
-
-import com.api.HotelBookingApp.Model.Feedback;
-
-public interface FeedbackService {
-
-	Object addFeedback(Feedback param) throws Exception;
-	Object getFeedback(Integer userId, Integer hotelId) throws Exception;
-}
